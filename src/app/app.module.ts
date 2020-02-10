c7c8cbdd07a509d01d2b7291207c45d319e3a4d9
Prashant Singh <prasingh26@publicisgroupe.net>
2020-02-10 16:22:11 +0530

    Initial Project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import  { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './productList/product.list.component';
+import { ProductComponent } from './productList/product/product.component';
+import { ProductDetailComponent } from './productList/product/detail/product.component.detail';
+import { ProductReviewComponent } from './productList/product/review/review.component';
+import { ProductService } from './productList/product.service';
+import { HttpClientModule }   from '@angular/common/http';
+import { NgbModule, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    ProductReviewComponent
+  ],
+  entryComponents: [
+    ProductDetailComponent,
+    ProductReviewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [ProductService, NgbModalConfig, NgbModal],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
